6b48e5ab881816e47ef900e8365b834a7a7dce68
y-solb <ysolb99@gmail.com>
2021-04-15 22:55:41 +0900

    HTML,CSS: navbar

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,12 @@
-import "./app.css";
+import './app.css';
+import Navbar from './components/navbar';
 
 function App() {
-  return <h1>Hello!</h1>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
