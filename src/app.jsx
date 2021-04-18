0dc606c2dbdbb29e0db753dafa5f62583c286cf0
y-solb <ysolb99@gmail.com>
2021-04-18 14:56:56 +0900

    search 기능 구현

--- src/app.jsx
+++ src/app.jsx
@@ -6,6 +6,22 @@ import VideoList from './components/video_list/video_list';
 function App() {
   const [videos, setVideos] = useState([]);
 
+  const search = (query) => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyD2yFwzDZFKZBTuzn5COdXaD3fuHNNyDfE`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => result.items.map((item) => ({ ...item, id: item.id.videoId })))
+      .then((items) => setVideos(items))
+      .catch((error) => console.log('error', error));
+  };
+
   useEffect(() => {
     const requestOptions = {
       method: 'GET',
@@ -21,10 +37,10 @@ function App() {
       .catch((error) => console.log('error', error));
   }, []);
   return (
-    <>
-      <Navbar />
+    <div className={styles.app}>
+      <Navbar onSearch={search} />
       <VideoList videos={videos} />
-    </>
+    </div>
   );
 }
 
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styles from './navbar.module.css';
 
-const Navbar = (props) => (
-  <nav className={styles.navbar}>
-    <img className={styles.logo} src="/imgs/youtube_logo.png" alt="youtube_logo" />
-    <form className={styles.searchForm}>
-      <input type="text" className={styles.searchInput} placeholder="Search" />
-      <button className={styles.searchBtn}>
-        <i class="fas fa-search"></i>
-      </button>
-    </form>
-  </nav>
-);
+const Navbar = ({ onSearch }) => {
+  const inputRef = useRef();
+
+  const handleSearch = () => {
+    const value = inputRef.current.value;
+    onSearch(value);
+  };
+
+  const onClick = () => {
+    handleSearch();
+  };
+  const onKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      handleSearch();
+    }
+  };
+
+  return (
+    <nav className={styles.navbar}>
+      <img className={styles.logo} src="/imgs/youtube_logo.png" alt="youtube_logo" />
+      <div className={styles.searchForm}>
+        <input
+          ref={inputRef}
+          className={styles.searchInput}
+          type="search"
+          placeholder="Search"
+          onKeyPress={onKeyPress}
+        />
+        <button className={styles.searchBtn} type="submit" onClick={onClick}>
+          <i className="fas fa-search"></i>
+        </button>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
