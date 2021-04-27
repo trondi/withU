b7f970fa8fa50b2826835ac67d6ea62f7f8682b6
y-solb <ysolb99@gmail.com>
2021-04-27 13:23:37 +0900

    반응형, css 수정

--- src/components/video_detail/video_detail.jsx
+++ src/components/video_detail/video_detail.jsx
@@ -3,19 +3,21 @@ import styles from './video_detail.module.css';
 
 const VideoDetail = ({ video, video: { snippet } }) => (
   <section className={styles.detail}>
-    <iframe
-      className={styles.video}
-      type="text/html"
-      title="youtube video player"
-      width="100%"
-      height="500px"
-      src={`https://www.youtube.com/embed/${video.id}`}
-      frameBorder="0"
-      allowFullScreen
-    ></iframe>
-    <h2>{snippet.title}</h2>
-    <h3>{snippet.channelTitle}</h3>
-    <pre className={styles.description}>{snippet.description}</pre>
+    <div className={styles.box}>
+      <iframe
+        className={styles.video}
+        type="text/html"
+        title="youtube video player"
+        width="100%"
+        height="500px"
+        src={`https://www.youtube.com/embed/${video.id}`}
+        frameBorder="0"
+        allowFullScreen
+      ></iframe>
+      <h3>{snippet.title}</h3>
+      <h5 className={styles.title}>{snippet.channelTitle}</h5>
+      <pre className={styles.description}>{snippet.description}</pre>
+    </div>
   </section>
 );
 
