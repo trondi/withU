c15cc7ce25eb5e9eb04b9ef44fdf9e62ce90cf1a
y-solb <ysolb99@gmail.com>
2021-06-03 19:45:32 +0900

    feat : trainer 생성

---
+++ src/components/trainer/trainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './trainer.module.css';
+
+const Trainer = (props) => {
+  return (
+    <div>
+      <h1>여기에 소스 입력</h1>
+    </div>
+  );
+};
+
+export default Trainer;
